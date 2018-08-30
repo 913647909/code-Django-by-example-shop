c9f7c62227d832a1e84fdada4c118d8c2fcd06c7
MisakaTang <mikasatang@gmail.com>
2018-08-30 11:00:49 +0800

    优惠券模块

--- cart/views.py
+++ cart/views.py
@@ -6,6 +6,7 @@ from django.views.decorators.http import require_POST
 from cart.cart import Cart
 from cart.forms import CartAddProductForm
 from shop.models import Product
+from coupons.forms import CouponApplyForm
 
 
 @require_POST
@@ -30,4 +31,5 @@ def cart_detail(request):
     cart = Cart(request)
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'update': True})
-    return render(request, 'cart/detail.html', {'cart': cart})
+    coupon_apply_form = CouponApplyForm()
+    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form})
